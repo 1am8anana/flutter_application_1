c0798918a3152a693441a4a5b436d7bfd5e1a084
Krittapas Wongchai <1am8anana@gmail.com>
2024-06-29 22:07:51 +0700

    Create 3 containers wit hdiffernt properties.

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -8,16 +8,14 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       drawer: const Drawer(child: Text("First Drawer")),
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('Main Screen'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context)
-                .push(MaterialPageRoute(builder: (context) => SecondScreen()));
-          },
-          child: const Text('Go to Second Screen'),
-        ),
+      body: Container(
+        child: Row(children: [
+          Container(color: Colors.red, height: 120, width: 120),
+          Container(color: Colors.green, height: 120, width: 200),
+          Container(color: Colors.blue, height: 150, width: 120)
+        ]),
       ),
     );
   }
